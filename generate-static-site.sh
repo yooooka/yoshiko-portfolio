eddb3ea9d08694ef15f2150cfa7a7f93644040ff
joe wright <joejoejoew@gmail.com>
2013-03-29 16:07:36 +0900

    tweaks

--- generate-static-site.sh
+++ generate-static-site.sh
@@ -2,10 +2,11 @@
 
 #wget -rkp -l3 -np -nH --cut-dirs=1 http://yoshiko.901farm.kaki/
 
+# -nH Disable generation of host-prefixed directories.
 # -p download page requisites
 # -r recursive
 # -k convert links so they are suitable for local viewing
 # -np no parent (only download below a dir for example)
 #wget -rkp -np --no-proxy http://yoshiko.901farm.kaki/
 
-wget --mirror -p --convert-links --no-proxy -P ./yoshiko-portfolio-deploy http://yoshiko.901farm.kaki/
+wget --mirror -p --convert-links --no-proxy -nH -k -P ./yoshiko-portfolio-deploy http://yoshiko.901farm.kaki/
