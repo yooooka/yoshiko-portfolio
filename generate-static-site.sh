606c5aca037f5b4398831315f1f9e95166262bde
joe wright <joejoejoew@gmail.com>
2013-03-29 15:43:29 +0900

    tweaked generate script and sample apache config

--- generate-static-site.sh
+++ generate-static-site.sh
@@ -8,5 +8,4 @@
 # -np no parent (only download below a dir for example)
 #wget -rkp -np --no-proxy http://yoshiko.901farm.kaki/
 
-
-wget --mirror -p --convert-links --no-proxy -P ./yoshiko.901farm.com http://yoshiko.901farm.kaki/
+wget --mirror -p --convert-links --no-proxy -P ./yoshiko-portfolio-deploy http://yoshiko.901farm.kaki/
